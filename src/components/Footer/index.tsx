db399805e4576e8847b67d409ed2dc361a187fbb
Henry Millison <dev@henrymillison.com>
2023-06-21 18:41:17 -0400

    hyperlink footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,6 +5,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 import styles from './styles.module.css';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import Link from '@docusaurus/Link';
 
 interface FooterProps {
   hideCTA?: boolean;
@@ -15,19 +16,19 @@ export default function Footer({ className, hideCTA }: FooterProps): JSX.Element
   const {siteConfig} = useDocusaurusContext();
   return (
     <footer className={clsx(styles.footerWrapper, className)}>
-      {hideCTA ? null : <button className={clsx(styles.cta, "bg-transparent border-none")} />}
+      {hideCTA ? null : <Link href="/waitlist" className={clsx(styles.cta, "bg-transparent border-none")} />}
       <div className="flex flex-col flex-1 justify-center">
         <p className={styles.footerCallout}>
           We Believe Everything Is <br /> Possible With <span className="font-bold">POSTGRES</span>
         </p>
-        <a className={clsx(styles.joinCTA, "mx-auto mt-28")} href="./waitlist.html"></a>
+        <Link className={clsx(styles.joinCTA, "mx-auto mt-28")} href="https://tembo.breezy.hr/" />
       </div>
       <div className={styles.footerLinks}>
-        <a className={clsx(styles.link, "dark:text-black text-white")}>Careers</a>
+        <Link href="https://tembo.breezy.hr/" className={clsx(styles.link, "dark:text-black text-white")}>Careers</Link>
         <img src={useBaseUrl("img/footer-separator.svg")} />
-        <a className={clsx(styles.link, "dark:text-black text-white")}>Trunk</a>
+        <Link href="https://pgtrunk.io/" className={clsx(styles.link, "dark:text-black text-white")}>Trunk</Link>
         <img src={useBaseUrl("img/footer-separator.svg")} />
-        <a className={clsx(styles.link, "dark:text-black text-white")}>Docs</a>
+        <Link href="/docs" className={clsx(styles.link, "dark:text-black text-white")}>Docs</Link>
         <img src={useBaseUrl("img/footer-separator.svg")} />
         <a className={clsx(styles.link, "dark:text-black text-white")}>Twitter</a>
         <img src={useBaseUrl("img/footer-separator.svg")} />
