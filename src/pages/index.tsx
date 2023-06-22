c75d35cc233e09fb9917cd4004fdbbeede3c7d70
Ryan Boland <bolandryanm@gmail.com>
2023-06-22 18:00:08 -0400

    eliminate usage of absolute positioning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home(): JSX.Element {
   return (
     <LayoutBackdrop>
       <div className="relative">
-        <section className="content-margin overflow-x-hidden">
+        <section className="content-margin">
           <p className="text-xl mb-2 font-body font-semibold">Goodbye Database Sprawl</p>
           <h1 className={styles.headline}>
             Hello<br />
@@ -38,15 +38,15 @@ export default function Home(): JSX.Element {
           <a className={clsx(styles.cta, styles.heroCTA, "absolute")} href="./waitlist.html"></a>
           <SolarSystem />
         </section>
-        <section className={clsx(styles.section, styles.content, "relative")}>
-          <img className={clsx(styles.screenshot, styles.screenshot1, "z-20 absolute")} src={useBaseUrl("img/tembo-screenshot-1.png")} />
-          <div className="text-center pt-0 pb-32">
+        <section className={clsx(styles.content)}>
+          <img className={clsx(styles.screenshot, styles.screenshot1, "z-20")} src={useBaseUrl("img/tembo-screenshot-1.png")} />
+          <div className={clsx(styles.goalText)}>
             <p className="text-base mb-4 font-display font-bold uppercase">Our Goal</p>
             <h2 className="md:text-5xl sm:text-3xl text-xl font-display font-normal">Build a database for every need.<div className="font-bold">Tembo makes it easy.</div></h2>
           </div>
-          <img className={clsx(styles.screenshot, styles.screenshot2, "bottom-0 absolute")} src={useBaseUrl("img/tembo-screenshot-2.png")}/>
+          <img className={clsx(styles.screenshot, styles.screenshot2)} src={useBaseUrl("img/tembo-screenshot-2.png")}/>
         </section>
-        <Footer className='absolute' />
+        <Footer className={clsx(styles.indexFooter, 'relative')} />
       </div>
     </LayoutBackdrop>
   );
