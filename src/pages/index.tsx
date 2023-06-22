42d21a89482c749f8e7caf6242611fe8f6b53ba1
Henry Millison <dev@henrymillison.com>
2023-06-22 11:52:06 -0400

    properly load in font weights for fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
           <img className={clsx(styles.screenshot, styles.screenshot1, "z-20 absolute")} src={useBaseUrl("img/tembo-screenshot-1.png")} />
           <div className="text-center pt-0 pb-32">
             <p className="text-base mb-4 font-display font-bold uppercase">Our Goal</p>
-            <h2 className="md:text-5xl sm:text-3xl text-xl font-display">Build a database for every need.<div className="font-bold">Tembo makes it easy.</div></h2>
+            <h2 className="md:text-5xl sm:text-3xl text-xl font-display font-normal">Build a database for every need.<div className="font-bold">Tembo makes it easy.</div></h2>
           </div>
           <img className={clsx(styles.screenshot, styles.screenshot2, "bottom-0 absolute")} src={useBaseUrl("img/tembo-screenshot-2.png")}/>
         </section>
