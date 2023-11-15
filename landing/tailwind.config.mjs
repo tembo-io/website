fd9d126b68c1929f7672a2167d746119b1f07ef5
Darren Baldwin <68653294+DarrenBaldwin07@users.noreply.github.com>
2023-11-15 17:02:26 -0500

    Restructure (#186)
    * colors + fonts
    
    * readme
    
    * add build scripts
    
    * updates
    
    * updates
    
    * updates
    
    * add product page
    
    * add pricing page
    
    ---------
    
    Co-authored-by: Darren B <68653294+Devd0@users.noreply.github.com>

---
+++ landing/tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				primary: ['Bai Jamjuree'],
+			},
+			colors: {
+				neon: '#E9FC87',
+				lightBlue: '#45DEF2',
+				blue: '#432F94',
+				purple: '#DB39CB',
+				pink: '#FA467B',
+				salmon: '#FF7D7F',
+				mwasi: '#1C1C1C',
+				darkGrey: '#414141',
+				grey: '#9EA2A6',
+				lightGrey: '#BBBBBB',
+			},
+		},
+	},
+	plugins: [],
+};
