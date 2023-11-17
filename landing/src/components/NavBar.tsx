2028d95dc8047342d0f910be327f1de1889b5c26
Darren B <68653294+Devd0@users.noreply.github.com>
2023-11-17 18:04:11 -0500

    init header

--- landing/src/components/NavBar.tsx
+++ landing/src/components/NavBar.tsx
@@ -14,12 +14,12 @@ const NavBar: React.FC<Props> = ({ currentPage }) => {
         <div className='flex justify-between items-center py-8'>
           <img src='/logoWithText.svg' alt='tembo log' width={124} />
           <div className='flex items-center gap-12'>
-            <a href="/" className={cx('font-secondary font-medium', currentPage == '/' ? 'text-neon' : 'text-white')}>Home</a>
-            <a href="/product" className={cx('font-secondary font-medium', currentPage == '/product' ? 'text-neon' : 'text-white')}>Product</a>
-            <a href="/pricing" className={cx('font-secondary font-medium', currentPage == '/pricing' ? 'text-neon' : 'text-white')}>Pricing</a>
-            <a href="/blog" className={cx('font-secondary font-medium', currentPage == '/blog' ? 'text-neon' : 'text-white')}>Blog</a>
+            <a href="/" className={cx('font-secondary font-medium', currentPage == '/' ? 'text-neon' : 'text-white opacity-70')}>Home</a>
+            <a href="/product" className={cx('font-secondary font-medium', currentPage == '/product' ? 'text-neon' : 'text-white opacity-70')}>Product</a>
+            <a href="/pricing" className={cx('font-secondary font-medium', currentPage == '/pricing' ? 'text-neon' : 'text-white opacity-70')}>Pricing</a>
+            <a href="/blog" className={cx('font-secondary font-medium', currentPage == '/blog' ? 'text-neon' : 'text-white opacity-70')}>Blog</a>
           </div>
-          <a href="https://cloud.tembo.io"><button className='bg-neon'>Try Free</button></a>
+          <a href="https://cloud.tembo.io"><button className='bg-neon hover:bg-[#D1E278] transition-all duration-150 ease-in font-semibold py-4 px-8 rounded-full font-secondary'>Try Free</button></a>
         </div>
       </Container>
     </div>
