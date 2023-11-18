d95b2e3847dab81216764763b1bb5805c96fee99
Darren B <68653294+Devd0@users.noreply.github.com>
2023-11-18 10:23:19 -0500

    confetti

--- landing/src/components/NavBar.tsx
+++ landing/src/components/NavBar.tsx
@@ -9,18 +9,18 @@ interface Props {
 
 const NavBar: React.FC<Props> = ({ currentPage }) => {
   return (
-    <div>
+    <div className='z-100'>
       <div className='bg-gradient-rainbow h-[4px] w-full' />
       <Container>
         <div className='flex justify-between items-center py-8'>
           <img src='/logoWithText.svg' alt='tembo log' width={124} />
           <div className='flex items-center gap-12'>
-            <a href="/" className={cx('font-secondary font-medium', currentPage == '/' ? 'text-neon' : 'text-white opacity-70')}>Home</a>
-            <a href="/product" className={cx('font-secondary font-medium', currentPage == '/product' ? 'text-neon' : 'text-white opacity-70')}>Product</a>
-            <a href="/pricing" className={cx('font-secondary font-medium', currentPage == '/pricing' ? 'text-neon' : 'text-white opacity-70')}>Pricing</a>
-            <a href="/blog" className={cx('font-secondary font-medium', currentPage == '/blog' ? 'text-neon' : 'text-white opacity-70')}>Blog</a>
+            <a href="/" className={cx('font-secondary font-medium z-10', currentPage == '/' ? 'text-neon' : 'text-white opacity-70')}>Home</a>
+            <a href="/product" className={cx('font-secondary font-medium z-10', currentPage == '/product' ? 'text-neon' : 'text-white opacity-70')}>Product</a>
+            <a href="/pricing" className={cx('font-secondary font-medium z-10', currentPage == '/pricing' ? 'text-neon' : 'text-white opacity-70')}>Pricing</a>
+            <a href="/blog" className={cx('font-secondary font-medium z-10', currentPage == '/blog' ? 'text-neon' : 'text-white opacity-70')}>Blog</a>
           </div>
-          <a href="https://cloud.tembo.io"><Button variant='neon'>Try Free</Button></a>
+          <a href="https://cloud.tembo.io"><Button variant='neon' styles='z-100'>Try Free</Button></a>
         </div>
       </Container>
     </div>
