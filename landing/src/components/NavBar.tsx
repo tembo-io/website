3756a5badebf4e53b4a12630197b3e3fdff12292
Darren B <68653294+Devd0@users.noreply.github.com>
2023-11-19 18:02:36 -0500

    finish navbar blur with bottom gradient border

--- landing/src/components/NavBar.tsx
+++ landing/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import Container from '../components/Container';
 import Button from './Button';
 import cx from 'classnames';
@@ -8,11 +8,25 @@ interface Props {
 }
 
 const NavBar: React.FC<Props> = ({ currentPage }) => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+			setScrollY(window.scrollY);
+		};
+
+		handleScroll();
+		window.addEventListener('scroll', handleScroll);
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+  }, []);
+
   return (
-    <div className='z-100'>
+    <div className={cx('fixed top-0 w-full z-50 transition duration-100', scrollY > 20 ? 'backdrop-blur-lg safari-blur' : '')} >
       <div className='bg-gradient-rainbow h-[4px] w-full' />
       <Container>
-        <div className='flex justify-between items-center py-8'>
+        <nav className={cx('flex justify-between items-center transition-all duration-100', scrollY > 20 ? 'py-4' : 'py-8')}>
           <a href="/"><img src='/logoWithText.svg' alt='tembo log' width={124} /></a>
           <div className='flex items-center gap-12'>
             <a href="/" className={cx('font-secondary font-medium z-10', currentPage == '/' ? 'text-neon' : 'text-white opacity-70')}>Home</a>
@@ -21,8 +35,9 @@ const NavBar: React.FC<Props> = ({ currentPage }) => {
             <a href="/blog" className={cx('font-secondary font-medium z-10', currentPage == '/blog' ? 'text-neon' : 'text-white opacity-70')}>Blog</a>
           </div>
           <a href="https://cloud.tembo.io"><Button variant='neon' styles='z-100'>Try Free</Button></a>
-        </div>
+        </nav>
       </Container>
+      <div className={cx('absolute bottom-0 flex h-[1px] w-full flex-row items-center justify-center opacity-100 shine', scrollY > 20 ? 'flex' : 'hidden' )} />
     </div>
   )
 }
