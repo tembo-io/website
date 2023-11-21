9b91a3c6b93bed474b7d740c1ad0e89540856c18
Darren B <68653294+Devd0@users.noreply.github.com>
2023-11-20 22:44:07 -0500

    updates

--- landing/src/components/StackSlider.tsx
+++ landing/src/components/StackSlider.tsx
@@ -4,35 +4,35 @@ import Card from './Card';
 const FirstStackCards = () => {
     return (
         <ul className='flex gap-4 items-center justify-center animate-infinite-scroll'>
-            <Card styles='bg-salmon flex flex-col justify-center min-w-[342px] h-[342px] rounded-3xl relative overflow-hidden'>
+            <Card styles='bg-salmon flex flex-col justify-center min-w-[342px] h-[342px] rounded-2xl relative overflow-hidden'>
                 <span className='z-10 mt-12'>
                     <p>Stack</p>
                     <h2 className='font-bold text-2xl mt-2'>Real-time inference service</h2>
                 </span>
                 <img src='/elephant2.svg' alt='elephant' className='absolute w-[215px] -top-[10px] -right-[60px] -z-0' />
             </Card>
-            <Card styles='bg-neon flex flex-col justify-start min-w-[342px] h-[342px] rounded-3xl relative overflow-hidden'>
+            <Card styles='bg-neon flex flex-col justify-start min-w-[342px] h-[342px] rounded-2xl  relative overflow-hidden'>
                 <span className='z-10'>
                     <p>Document Stack</p>
                     <h2 className='font-bold text-2xl mt-2'>Mobile app backend</h2>
                 </span>
                 <img src='/elephant7.svg' alt='elephant' className='absolute w-[215px] -bottom-[10px] -right-[30px] -z-0' />
             </Card>
-            <Card styles='bg-semiGrey flex flex-col justify-start min-w-[342px] h-[342px] rounded-3xl relative overflow-hidden'>
+            <Card styles='bg-semiGrey flex flex-col justify-start min-w-[342px] h-[342px] rounded-2xl relative overflow-hidden'>
                 <span className='z-10 text-white'>
                     <p>Machine Learning Stack</p>
                     <h2 className='font-bold text-2xl mt-2'>ML Training Stack</h2>
                 </span>
                 <img src='/elephant6.svg' alt='elephant' className='absolute w-[215px] -bottom-[10px] -right-[20px] -z-0 mirror-horizontal' />
             </Card>
-            <Card styles='bg-offBlack flex flex-col justify-start min-w-[342px] h-[342px] rounded-3xl relative overflow-hidden'>
+            <Card styles='bg-offBlack flex flex-col justify-start min-w-[342px] h-[342px] rounded-2xl relative overflow-hidden'>
                 <span className='z-10 text-white'>
                     <p>Message Queue Stack</p>
                     <h2 className='font-bold text-2xl mt-2'>SQS Replacement System</h2>
                 </span>
                 <img src='/elephant9.svg' alt='elephant' className='absolute w-[215px] -bottom-[10px] -right-[20px] -z-0 mirror-horizontal' />
             </Card>
-            <Card styles='bg-purple flex flex-col justify-center min-w-[342px] h-[342px] rounded-3xl relative overflow-hidden'>
+            <Card styles='bg-purple flex flex-col justify-center min-w-[342px] h-[342px] rounded-2xl relative overflow-hidden'>
                 <span className='z-10 mt-12'>
                     <p>OLAP Stack</p>
                     <h2 className='font-bold text-2xl mt-2'>Data Warehouse for Analytics</h2>
@@ -46,35 +46,35 @@ const FirstStackCards = () => {
 const SecondStackCards = () => {
     return (
         <ul className='flex gap-4 items-center justify-center animate-infinite-scroll-fast'>
-            <Card styles='bg-salmon flex flex-col justify-center min-w-[342px] h-[342px] rounded-3xl relative overflow-hidden'>
+            <Card styles='bg-salmon flex flex-col justify-center min-w-[342px] h-[342px] rounded-2xl relative overflow-hidden'>
                 <span className='z-10 mt-12'>
                     <p>Stack</p>
                     <h2 className='font-bold text-2xl mt-2'>Real-time inference service</h2>
                 </span>
                 <img src='/elephant2.svg' alt='elephant' className='absolute w-[215px] -top-[10px] -right-[60px] -z-0' />
             </Card>
-            <Card styles='bg-neon flex flex-col justify-start min-w-[342px] h-[342px] rounded-3xl relative overflow-hidden'>
+            <Card styles='bg-neon flex flex-col justify-start min-w-[342px] h-[342px] rounded-2xl relative overflow-hidden'>
                 <span className='z-10'>
                     <p>Document Stack</p>
                     <h2 className='font-bold text-2xl mt-2'>Mobile app backend</h2>
                 </span>
                 <img src='/elephant7.svg' alt='elephant' className='absolute w-[215px] -bottom-[10px] -right-[30px] -z-0' />
             </Card>
-            <Card styles='bg-semiGrey flex flex-col justify-start min-w-[342px] h-[342px] rounded-3xl relative overflow-hidden'>
+            <Card styles='bg-semiGrey flex flex-col justify-start min-w-[342px] h-[342px] rounded-2xl relative overflow-hidden'>
                 <span className='z-10 text-white'>
                     <p>Machine Learning Stack</p>
                     <h2 className='font-bold text-2xl mt-2'>ML Training Stack</h2>
                 </span>
                 <img src='/elephant6.svg' alt='elephant' className='absolute w-[215px] -bottom-[10px] -right-[20px] -z-0 mirror-horizontal' />
             </Card>
-            <Card styles='bg-offBlack flex flex-col justify-start min-w-[342px] h-[342px] rounded-3xl relative overflow-hidden'>
+            <Card styles='bg-offBlack flex flex-col justify-start min-w-[342px] h-[342px] rounded-2xl relative overflow-hidden'>
                 <span className='z-10 text-white'>
                     <p>Message Queue Stack</p>
                     <h2 className='font-bold text-2xl mt-2'>SQS Replacement System</h2>
                 </span>
                 <img src='/elephant9.svg' alt='elephant' className='absolute w-[215px] -bottom-[10px] -right-[20px] -z-0 mirror-horizontal' />
             </Card>
-            <Card styles='bg-purple flex flex-col justify-center min-w-[342px] h-[342px] rounded-3xl relative overflow-hidden'>
+            <Card styles='bg-purple flex flex-col justify-center min-w-[342px] h-[342px] rounded-2xl relative overflow-hidden'>
                 <span className='z-10 mt-12'>
                     <p>OLAP Stack</p>
                     <h2 className='font-bold text-2xl mt-2'>Data Warehouse for Analytics</h2>
