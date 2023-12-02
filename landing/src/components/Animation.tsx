e957e857d99d19c597fab8848b0cb3953cfbce9f
Darren B <68653294+Devd0@users.noreply.github.com>
2023-12-01 22:18:35 -0500

    finish base stack section

--- landing/src/components/Animation.tsx
+++ landing/src/components/Animation.tsx
@@ -1,25 +1,37 @@
-import React, { useRef, useEffect } from 'react';
-import Lottie, { useLottie, type LottieRefCurrentProps  } from 'lottie-react';
+import React, { useRef, useEffect, useState } from 'react';
+import Lottie, { useLottie, type LottieRefCurrentProps } from 'lottie-react';
+import { useIntersection } from '../util';
 
 interface Props {
 	animation: unknown;
 	styles?: string;
 	isFullWidth?: boolean;
+	loop?: boolean;
+	animateOnInView?: boolean;
+	autoPlay?: boolean;
 }
 
-const Animation: React.FC<Props> = ({ animation, styles, isFullWidth = true }) => {
+const Animation: React.FC<Props> = ({ animation, styles, isFullWidth = true, loop = true, animateOnInView = false }) => {
+	const triggerRef = useRef(null);
+    const lottieRef = useRef(null)
+    const isVisible = useIntersection(triggerRef, "700px");
 	return (
-		<Lottie
-			animationData={animation}
-			loop={false}
-			className={styles}
-			style={{
-				position: 'absolute',
-				width: isFullWidth ? '100%' : undefined,
-				overflow: 'hidden',
-				zIndex: 1
-			}}
-		/>
+		<div ref={triggerRef}>
+			{!isVisible && animateOnInView? <></> : <Lottie
+				lottieRef={lottieRef}
+				animationData={animation}
+				loop={loop}
+				autoPlay={false}
+				className={styles}
+				style={{
+					position: 'absolute',
+					width: isFullWidth ? '100%' : undefined,
+					overflow: 'hidden',
+					zIndex: 1
+				}}
+			/>}
+
+		</div>
 	)
 }
 
---
+++ landing/src/util.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export const useIntersection = (element: any, rootMargin: string) => {
+  const [isVisible, setState] = useState(false);
+
+  useEffect(() => {
+    const current = element?.current;
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setState(entry.isIntersecting);
+      },
+      { rootMargin }
+    );
+    current && observer?.observe(current);
+
+    return () => current && observer.unobserve(current);
+  }, []);
+
+  return isVisible;
+};
