04cc658cc0b038a062e28303b3bdddd4ceaeccbc
Darren B <68653294+Devd0@users.noreply.github.com>
2023-12-06 10:21:34 -0500

    finish why tembo section

--- landing/src/components/DetailSequence.tsx
+++ landing/src/components/DetailSequence.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import cx from 'classnames';
 
 interface Props {
   iconPath?: string;
   header: string;
   text: string;
-
+  styles?: string;
 }
 
-const DetailSequence: React.FC<Props> = ({ iconPath, header, text }) => {
+const DetailSequence: React.FC<Props> = ({ iconPath, header, text, styles }) => {
   return (
-    <div className='flex flex-col gap-4 max-w-[530px]'>
+    <div className={cx('flex flex-col gap-4 max-w-[530px]', styles)}>
       {iconPath && <img src={iconPath} alt='Colored Icon' className='w-[56px] h-[56px]' />}
       <h1 className='text-white font-semibold font-secondary text-xl'>{header}</h1>
       <p className='font-secondary font-medium text-base text-otherGrey opacity-70'>{text}</p>
