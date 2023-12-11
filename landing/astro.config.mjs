8e361794ec26c7f01832fb7c1fe43549cb0d4f00
Darren B <68653294+Devd0@users.noreply.github.com>
2023-12-11 18:51:10 -0500

    update hero styles

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -5,4 +5,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  redirects: {
+    '/product': '/',
+    '/pricing': '/'
+  }
 });
