d2775eaf107d61f14c032d93d98c409f530b5a74
Darren Baldwin <68653294+DarrenBaldwin07@users.noreply.github.com>
2023-12-14 12:04:50 -0500

    Finish home page (#218)
    * fix stack slider styles
    
    * updates
    
    * more animation work
    
    * updates
    
    * finish stack slider animations
    
    * add app screenshot image and start DetailSequence comp
    
    * start on Tembo + stacks side description
    
    * finish base stack section
    
    * updates
    
    * updates
    
    * fade out confetti animation
    
    * make stackSlider size smaller
    
    * fix lottie bild error
    
    * bunlde docs + blog with landing & update build scripts
    
    * updates
    
    * update move-docs script
    
    * test with wrong command
    
    * revert
    
    * rsync
    
    * back to cp
    
    * fix out dir
    
    * update cp cmd
    
    * fix res of noise background
    
    * finish extensions section && start on footer
    
    * finish why tembo section
    
    * footer classname
    
    * rm animation and finish join the herd buttons
    
    * updates
    
    * update footer animation
    
    * add footer
    
    * test with header animation
    
    * animation delay
    
    * update careers link
    
    * updates
    
    * fix focus cut-off on button
    
    * slow down stack slider
    
    * checkpoint with animation
    
    * max width
    
    * update sizing
    
    * updated
    
    * breakpoint on tembo stack section temp fix
    
    * random stack colors in slider
    
    * update extension graphics
    
    * image paths
    
    * re-add images
    
    * re-add images
    
    * rename HeroStar.svg
    
    * update hero styles
    
    * change all ctas to try free
    
    * add custom breakpoints
    
    * fix conditional styles in HeroCard
    
    * navbar sizing on mobile
    
    * hero responsive
    
    * text size responsiveness for stack slider header container
    
    * confetti responsive
    
    * confetti position
    
    * hero image responsive
    
    * postgres section responsive
    
    * text center for 100% postgres section
    
    * postgres section responsive complete
    
    * tembo + stacks responsivenss
    
    * fix font size for TEMBO + Stacks
    
    * updates
    
    * fix light gradient on safari
    
    * formatting
    
    * footer responsiveness
    
    * unused import
    
    * packagelock fix
    
    * test
    
    * prettier
    
    * tweak confetti animation
    
    * update confetti in tembo stacks section when on mobile
    
    * fix build
    
    * hamburger menu
    
    * start on meta tags
    
    * updates
    
    * rm confetti hiding copy
    
    * finish mobile menu
    
    * updates
    
    * update outline border
    
    * update stack slider fonts
    
    * update og image
    
    * font sizes
    
    * add slight menu animation
    
    * hero copy + images
    
    * image height
    
    * fix image path
    
    * updates
    
    * fix stretched
    
    * fix buttons
    
    * update animation size
    
    * padding
    
    * more padding
    
    * update meta description
    
    * meta description
    
    * fix org image string
    
    * update app screenshot
    
    * update mobile menu
    
    * refresh hero
    
    * unused code
    
    * fix mobile menu
    
    * fix footer
    
    * add clearbit + plausable
    
    * hero card width
    
    * fix docs config
    
    * fix button in 100% postgres section
    
    ---------
    
    Co-authored-by: Darren B <68653294+Devd0@users.noreply.github.com>

--- landing/tailwind.config.mjs
+++ landing/tailwind.config.mjs
@@ -8,38 +8,53 @@ export default {
 				secondary: ['Inter']
 			},
 			maxWidth: {
-				container: '100rem',
+				container: '88rem',
+				stackSlider: '130rem'
 			},
 			colors: {
 				neon: '#E9FC87',
+				lightNeon: '#F3FFB6',
 				lightBlue: '#45DEF2',
 				blue: '#432F94',
 				purple: '#DB39CB',
+				lightPurple: '#CA6FE5',
 				pink: '#FA467B',
+				lightPink: '#FFC9EA',
 				salmon: '#FF7D7F',
 				mwasi: '#1C1C1C',
-				darkGrey: '#414141',
+				customDarkGrey: '#414141',
 				semiGrey: '#3E4243',
 				grey: '#9EA2A6',
 				lightGrey: '#BBBBBB',
+				otherGrey: '#EAEAEA',
 				offBlack: '#120F0E',
-				offGrey: '#1A1E22'
+				offGrey: '#1A1E22',
 			},
 			backgroundImage: {
 				'gradient-rainbow':
 					'linear-gradient(89.98deg, #FFC6D9 1.27%, #7CCFE1 34.18%, #89CBA6 64.52%, #A5D571 100%)',
-				'gradient-button': 'linear-gradient(68.42deg, #F77577 0%, #DB39CB 79.07%, #CA6FE5 170.36%)'
+				'gradient-button': 'linear-gradient(68.42deg, #F77577 0%, #DB39CB 79.07%, #CA6FE5 170.36%)',
+				'gradient-button-darker': 'linear-gradient(68.42deg, #F06B6D 0%, #C927A7 79.07%, #BA61D1 170.36%)'
 			},
 			animation: {
-				'infinite-scroll': 'infinite-scroll 25s linear infinite',
-				'infinite-scroll-fast': 'infinite-scroll 15s linear infinite'
+				'infinite-scroll': 'infinite-scroll 30s linear infinite',
+				'infinite-scroll-fast': 'infinite-scroll 20s linear infinite'
 			},
 			keyframes: {
 				'infinite-scroll': {
 					from: { transform: 'translateX(0)' },
 					to: { transform: 'translateX(-100%)' },
 				}
+			},
+			screens: {
+				customLg: '1440px',
+				customMd: '1280px',
+				customSm: '1024px',
+				mobile: '900px',
+				customXs: '680px',
+				customXxs: '375px'
 			}
+
 		}
 	},
 	plugins: []
