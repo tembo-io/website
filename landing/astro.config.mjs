e789b71dd163e4d4ea30bd1f548a9363aa774c18
Darren B <68653294+Devd0@users.noreply.github.com>
2023-12-13 20:36:09 -0500

    update meta description

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  redirects: {
-    '/product': '/',
-    '/pricing': '/'
-  }
+	integrations: [tailwind(), react()],
+	site: 'https://tembo.io',
+	redirects: {
+		'/product': '/',
+		'/pricing': '/',
+	},
 });
