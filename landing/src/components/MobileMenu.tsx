8caa214c0c5e513f4c5689675febf0d458d2d3ca
Darren B <68653294+Devd0@users.noreply.github.com>
2023-12-14 23:12:19 -0500

    move mobile buttons down to bottom

--- landing/src/components/MobileMenu.tsx
+++ landing/src/components/MobileMenu.tsx
@@ -7,45 +7,48 @@ const MobileMenu = () => {
 	return (
 		<div className='bg-offBlack mobile:hidden fixed z-10 w-full h-screen overflow-hidden inset-0'>
 			<div className='bg-gradient-rainbow h-[4px] w-full' />
-			<Container>
-				<nav className='flex flex-col gap-[32px] mt-28 h-full'>
-					<a
-						href='/'
-						className='font-secondary font-normal z-10 text-white text-[20px]'
-					>
-						Home
-					</a>
-					<img src={'/line.svg'} alt='line' />
-					<a
-						href='/docs'
-						className='font-secondary font-normal z-10 text-white text-[20px]'
-					>
-						Docs
-					</a>
-					<img src={'/line.svg'} alt='line' />
-					<a
-						href='/blog'
-						className='font-secondary font-normal z-10 text-white text-[20px]'
-					>
-						Blog
-					</a>
-					<img src={'/line.svg'} alt='line' />
-					<a
-						href='https://github.com/tembo-io'
-						target='_blank'
-						rel='noreferrer'
-						className='font-secondary font-normal z-10 text-white text-[20px]'
-					>
-						<div className='flex gap-2 items-center'>
-							<img
-								src={'/github.svg'}
-								alt='line'
-								width={23}
-								height={23}
-							/>
-							Github
-						</div>
-					</a>
+			<Container styles='h-[84%]'>
+				<nav className='flex flex-col gap-[32px] mt-28 h-full justify-between'>
+					<div className='flex flex-col gap-[32px]'>
+						<a
+							href='/'
+							className='font-secondary font-normal z-10 text-white text-[20px]'
+						>
+							Home
+						</a>
+						<img src={'/line.svg'} alt='line' />
+						<a
+							href='/docs'
+							className='font-secondary font-normal z-10 text-white text-[20px]'
+						>
+							Docs
+						</a>
+						<img src={'/line.svg'} alt='line' />
+						<a
+							href='/blog'
+							className='font-secondary font-normal z-10 text-white text-[20px]'
+						>
+							Blog
+						</a>
+						<img src={'/line.svg'} alt='line' />
+						<a
+							href='https://github.com/tembo-io'
+							target='_blank'
+							rel='noreferrer'
+							className='font-secondary font-normal z-10 text-white text-[20px]'
+						>
+							<div className='flex gap-2 items-center'>
+								<img
+									src={'/github.svg'}
+									alt='line'
+									width={23}
+									height={23}
+								/>
+								Github
+							</div>
+						</a>
+					</div>
+
 					<div className='flex flex-col gap-4'>
 						<Button
 							variant='gradient'
