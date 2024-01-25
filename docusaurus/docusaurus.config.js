fe65fbd61a1813276a71d583b52da5b2ae190563
Darren B <68653294+Devd0@users.noreply.github.com>
2024-01-25 10:05:03 -0500

    trigger

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -11,8 +11,6 @@ const config = {
 	favicon: 'favicon.ico',
 	// Set the production url of your site here
 	url: 'https://tembo.io',
-	// Set the /<baseUrl>/ pathname under which your site is served
-	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/',
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
