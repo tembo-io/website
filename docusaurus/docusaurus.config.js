eefbea07f7bb4ff9277e198d90f5a43b1222bd3f
Darren B <68653294+Devd0@users.noreply.github.com>
2024-01-25 12:08:45 -0500

    turn off trailing slash again

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -18,7 +18,6 @@ const config = {
 	projectName: 'website', // Usually your repo name.
 	onBrokenLinks: 'ignore',
 	onBrokenMarkdownLinks: 'ignore',
-	trailingSlash: false,
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
 	// to replace "en" with "zh-Hans".
