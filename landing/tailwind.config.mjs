4f67d9bca264c828708d6b1201ba7677f5b5423a
Darren B <68653294+Devd0@users.noreply.github.com>
2024-01-29 08:33:17 -0500

    updates

--- landing/tailwind.config.mjs
+++ landing/tailwind.config.mjs
@@ -4,6 +4,13 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			typography: {
+				DEFAULT: {
+				  css: {
+					maxWidth: '100ch', // add required value here
+				  }
+				}
+			},
 			fontFamily: {
 				primary: ['Bai Jamjuree'],
 				secondary: ['Inter']
