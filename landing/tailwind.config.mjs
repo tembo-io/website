7224d5773a8de8c56dfc1a3681b8c41fe9956c08
Darren B <68653294+Devd0@users.noreply.github.com>
2024-01-29 07:54:23 -0500

    style single code blocks in markdown

--- landing/tailwind.config.mjs
+++ landing/tailwind.config.mjs
@@ -1,3 +1,4 @@
+const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -58,7 +59,15 @@ export default {
 
 		}
 	},
-	plugins: [require('@tailwindcss/typography')]
+	plugins: [
+		require('@tailwindcss/typography'),
+		plugin(function ({addVariant}) {
+			addVariant(
+			  'prose-inline-code',
+			  '&.prose :where(:not(pre)>code):not(:where([class~="not-prose"] *))'
+			);
+		})
+	]
 };
 
 
