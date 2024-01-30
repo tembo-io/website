22b7555ac02eb0d82e0ab6dc96f19dfcf0ed6e69
Darren B <68653294+Devd0@users.noreply.github.com>
2024-01-30 10:32:25 -0500

    add padding to blog links instead of gap

--- landing/src/components/BlogLinks.tsx
+++ landing/src/components/BlogLinks.tsx
@@ -13,8 +13,8 @@ const BlogLinks: React.FC<Props> = ({ headings }) => {
     return (
         <div className='flex flex-col gap-6 max-w-[250px]'>
             <h2 className='text-lightGrey font-bold'>On this page</h2>
-            <div className='flex flex-col gap-4 border-l-2 border-[#9EA2A633]'>
-                {headings.map(({ text, slug }, index) => <a href={`#${slug}`} onClick={() => setLink(slug)} key={slug} className={cx('text-white font-secondary text-xs pl-4 border-l-2', link === slug ? 'border-neon' : 'border-transparent')}>{text}</a>)}
+            <div className='flex flex-col border-l-2 border-[#9EA2A633] gap-4'>
+                {headings.map(({ text, slug }, index) => <a href={`#${slug}`} onClick={() => setLink(slug)} key={slug} className={cx('text-white font-secondary text-xs pl-4 border-l-2 py-1', link === slug || link === '' && index === 0 ? 'border-neon' : 'border-transparent')}>{text}</a>)}
             </div>
         </div>
     )
