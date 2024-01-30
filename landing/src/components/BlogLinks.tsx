e58652d4848e9d3d1d8160f9a96221a7aa36624b
Darren B <68653294+Devd0@users.noreply.github.com>
2024-01-30 09:04:40 -0500

    add focus to BlogLinks

--- landing/src/components/BlogLinks.tsx
+++ landing/src/components/BlogLinks.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cx from 'classnames';
 
 interface Props {
@@ -9,16 +9,15 @@ interface Props {
 }
 
 const BlogLinks: React.FC<Props> = ({ headings }) => {
-  return (
-    <div className='flex flex-col min-w-[300px] items-end'>
-        <div className='flex flex-col gap-4 w-max'>
+    const [link, setLink] = useState(window.location.hash)
+    return (
+        <div className='flex flex-col gap-6 max-w-[250px]'>
             <h2 className='text-lightGrey font-bold'>On this page</h2>
-            <div className='flex flex-col gap-2 border-l-2 border-[#9EA2A633]'>
-                {headings.map(({ text, slug }) => <a href={`#${slug}`} className={cx('text-white font-secondary text-xs pl-4 border-l-2 border-transparent', window.location.href.includes(`${slug}`) ? 'border-neon' : 'border-transparent')}>{text}</a>)}
+            <div className='flex flex-col gap-4 border-l-2 border-[#9EA2A633]'>
+                {headings.map(({ text, slug }, index) => <a href={`#${slug}`} onClick={() => setLink(slug)} key={slug} className={cx('text-white font-secondary text-xs pl-4 border-l-2', link === slug ? 'border-neon' : 'border-transparent')}>{text}</a>)}
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default BlogLinks;
