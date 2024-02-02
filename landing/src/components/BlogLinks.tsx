d74a2bbba15e5e4817f34e8f2e1701cb6036fe98
Darren B <68653294+Devd0@users.noreply.github.com>
2024-02-02 10:02:50 -0500

    toc working

--- landing/src/components/BlogLinks.tsx
+++ landing/src/components/BlogLinks.tsx
@@ -11,32 +11,34 @@ interface Props {
 
 const useActiveAnchors = (
   anchorsQuerySelector: string = 'h2',
-  tocQuerySelector: string = '.prose-toc a',
-  offset: number = 200
+  tocQuerySelector: string = '.prose-toc',
+  offset: number = 120
 ) => {
   const anchors = useRef<NodeListOf<HTMLHeadingElement> | null>(null)
   const toc = useRef<NodeListOf<HTMLHeadingElement> | null>(null)
 
   const handleScroll = () => {
-    const pageYOffset = window.pageYOffset
-    let newActiveAnchor: string = ''
+    const pageYOffset = window.scrollY;
+    let newActiveAnchor: string = '';
 
     anchors.current?.forEach((anchor) => {
       if (pageYOffset >= anchor.offsetTop - offset) {
-        newActiveAnchor = anchor.id
+        if (!!anchor.id) {
+            newActiveAnchor = anchor.id
+        }
       }
     })
 
     toc.current?.forEach((link) => {
-      link.classList.remove('toc-animate')
-      // TODO: escape emojis
-      // The problem is that MDXRemote strips out emojis on slugs
-      // but ReactMarkdown doesn't,instead it encodes them
+      link.classList.remove('border-neon')
+      link.classList.add('border-transparent')
       const sanitizedHref = (link.getAttribute('href') ?? '').replace('#', '')
-      const isMatch = sanitizedHref === newActiveAnchor
+
+      const isMatch = sanitizedHref === newActiveAnchor;
 
       if (isMatch) {
-        link.classList.add('toc-animate')
+        link.classList.remove('border-transparent')
+        link.classList.add('border-neon')
       }
     })
   }
@@ -71,46 +73,7 @@ const BlogLinks: React.FC<Props> = ({ headings }) => {
         const element = document.getElementById(section);
         element?.scrollIntoView({ behavior: 'smooth' });
     };
-
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollPosition = window.scrollY;
-            const sections = document.querySelectorAll('h1, h2, h3, h4, h5, h6') as NodeListOf<HTMLHeadingElement>;
-
-            sections.forEach((section) => {
-                const sectionTop = section.offsetTop;
-                const sectionHeight = section.offsetHeight;
-
-                if (
-                    scrollPosition >= sectionTop &&
-                    scrollPosition < sectionTop + sectionHeight
-                ) {
-                    setLink(section.id);
-                }
-            });
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    useEffect(() => {
-        const handleUrlAnchorChange = () => {
-            const sectionFromUrl = window.location.hash.substring(1)
-            setLink(sectionFromUrl);
-        };
-
-        window.addEventListener('hashchange', handleUrlAnchorChange);
-        return () => {
-            window.removeEventListener('hashchange', handleUrlAnchorChange);
-        };
-    }, []);
-
-    useEffect(() => {
-        window.location.hash = link
-    }, [link])
+    const _ = useActiveAnchors();
 
     return (
         <div className='flex flex-col gap-6 max-w-[250px]'>
