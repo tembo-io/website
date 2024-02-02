c0db389edf0cb9366bf2d400ee3f5abd982ce258
Darren B <68653294+Devd0@users.noreply.github.com>
2024-02-02 08:38:13 -0500

    checkpoint

--- landing/src/components/BlogLinks.tsx
+++ landing/src/components/BlogLinks.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import cx from 'classnames';
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
 
 interface Props {
     headings: {
@@ -8,6 +7,54 @@ interface Props {
         slug: string
     }[]
 }
+
+
+const useActiveAnchors = (
+  anchorsQuerySelector: string = 'h2',
+  tocQuerySelector: string = '.prose-toc a',
+  offset: number = 200
+) => {
+  const anchors = useRef<NodeListOf<HTMLHeadingElement> | null>(null)
+  const toc = useRef<NodeListOf<HTMLHeadingElement> | null>(null)
+
+  const handleScroll = () => {
+    const pageYOffset = window.pageYOffset
+    let newActiveAnchor: string = ''
+
+    anchors.current?.forEach((anchor) => {
+      if (pageYOffset >= anchor.offsetTop - offset) {
+        newActiveAnchor = anchor.id
+      }
+    })
+
+    toc.current?.forEach((link) => {
+      link.classList.remove('toc-animate')
+      // TODO: escape emojis
+      // The problem is that MDXRemote strips out emojis on slugs
+      // but ReactMarkdown doesn't,instead it encodes them
+      const sanitizedHref = (link.getAttribute('href') ?? '').replace('#', '')
+      const isMatch = sanitizedHref === newActiveAnchor
+
+      if (isMatch) {
+        link.classList.add('toc-animate')
+      }
+    })
+  }
+
+  useEffect(() => {
+    anchors.current = document.querySelectorAll(anchorsQuerySelector)
+    toc.current = document.querySelectorAll(tocQuerySelector)
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return null
+}
+
 
 const BlogLinks: React.FC<Props> = ({ headings }) => {
     const [link, setLink] = useState('')
@@ -61,11 +108,15 @@ const BlogLinks: React.FC<Props> = ({ headings }) => {
         };
     }, []);
 
+    useEffect(() => {
+        window.location.hash = link
+    }, [link])
+
     return (
         <div className='flex flex-col gap-6 max-w-[250px]'>
             <h2 className='text-lightGrey font-bold'>On this page</h2>
             <div className='flex flex-col border-l-2 border-[#9EA2A633] gap-4'>
-                {headings.map(({ text, slug }, index) => <a href={`#${slug}`} key={slug} onClick={() => handleLinkClick(slug)} className={cx('text-white font-secondary text-xs pl-4 border-l-2 py-1', link === slug || link === '' && index === 0 ? 'border-neon' : 'border-transparent')}>{text}</a>)}
+                {headings.map(({ text, slug }, index) => <a href={`#${slug}`} key={slug} onClick={() => handleLinkClick(slug)} className={cx('text-white font-secondary text-xs pl-4 border-l-2 py-1 prose-toc', link === slug || link === '' && index === 0 ? 'border-neon' : 'border-transparent')}>{text}</a>)}
             </div>
         </div>
     )
