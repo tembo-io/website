eb9c040f71bbd30bf3fc6380198f82a74c6efa0e
Darren B <68653294+Devd0@users.noreply.github.com>
2024-02-05 20:03:19 -0500

    move atom feed to root

--- landing/src/pages/blog/atom.xml.js
+++ landing/src/pages/blog/atom.xml.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 
 
 export async function GET(context) {
-    const file = fs.readFileSync('./src/pages/blog/atom.xml', 'utf8')
+    const file = fs.readFileSync('atom.xml', 'utf8')
     return new Response(
         file
     )
