92eefec6e34736c91db23bf54880a08301a77b2e
Darren Baldwin <68653294+DarrenBaldwin07@users.noreply.github.com>
2024-02-07 10:43:19 -0500

    prettier formatting (#344)
    Co-authored-by: Darren B <68653294+Devd0@users.noreply.github.com>

--- landing/src/components/Logo.tsx
+++ landing/src/components/Logo.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Logo = () => {
-  return (
-    <a
-        href='/'
-        className='focus:outline-none transition hover:scale-105 duration-300 ease-in-out delay-70 z-50'
-    >
-        <img
-            src='/logoWithText.svg'
-            alt='tembo log'
-            className='w-[105px] mobile:w-[124px]'
-        />
-    </a>
-  )
-}
+	return (
+		<a
+			href='/'
+			className='focus:outline-none transition hover:scale-105 duration-300 ease-in-out delay-70 z-50'
+		>
+			<img
+				src='/logoWithText.svg'
+				alt='tembo log'
+				className='w-[105px] mobile:w-[124px]'
+			/>
+		</a>
+	);
+};
 
 export default Logo;
