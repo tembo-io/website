dbdfb408bebde83799a6eb6e04ee8f9228f25a53
Darren B <68653294+Devd0@users.noreply.github.com>
2024-02-07 07:28:14 -0500

    use compiledContent

--- landing/src/pages/feed.xml.js
+++ landing/src/pages/feed.xml.js
@@ -17,8 +17,6 @@ export async function GET(context) {
     const blog = await getCollection('blog');
     const postImportResult = import.meta.glob('../content/blog/**/*.md', { eager: true });
     const posts = Object.values(postImportResult);
-    console.log(sanitizeHtml(posts[5].compiledContent()))
-
     return rss({
         title: 'Tembo’s Blog',
         description: 'Latest news and technical blog posts from membors of the Tembo team and community!',
@@ -27,34 +25,17 @@ export async function GET(context) {
             atom: "http://www.w3.org/2005/Atom",
         },
         customData: `<atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />`,
-        items: blog.map((post) => {
+        items: blog.map((post, index) => {
             const dateString = post.id.substring(0, 10);
             const parsedDate = post.data?.date || new Date(dateString);
             const renderedMarkdown = parser.render(post.body);
-            const imgRegex = /<img.*?src=["'](.*?)["'].*?>/g;
             const isMdx = post.id.includes('.mdx');
-            const postImageTags = [];
-            var match;
-            while ((match = imgRegex.exec(renderedMarkdown)) !== null) {
-                postImageTags.push(match[0]); // Push the whole matched tag
-            }
-
-            postImageTags.forEach(async (img) => {
-                const currentSrcValues = img.match(imgRegex).map(function(tag) {
-                    return tag.match(/src=["'](.*?)["']/)[1];
-                });
-                const imagePath = `../content/blog/${post.id.replace(isMdx ? '/index.mdx' : '/index.md', '')}${currentSrcValues[0].replace('.', '')}`
-                //const newImage = await getImage({src: import.meta.glob(imagePath)})
-                const cleanedImageTag = img.replace(imgRegex, function(match, captureGroup) {
-                    return match.replace(captureGroup, 'hello-world');
-                });
-            })
             return {
                 title: post.data.title,
                 pubDate: new Date(parsedDate).toISOString(),
                 description: post.data.description,
                 link: `/blog/${post.slug}`,
-                content: renderedMarkdown.replace('src="./', `src="${context.site}./`),
+                content: isMdx ? 'Could not be rendered!' : posts[index]?.compiledContent() || 'Could not be rendered!',
                 customData: `
                     <author>
                         <name>${AUTHORS[post.data.authors[0]].name}</name>
