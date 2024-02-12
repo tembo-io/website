740b68017c966b529b44a862f03b588392d6187c
Darren B <68653294+Devd0@users.noreply.github.com>
2024-02-12 08:45:06 -0500

    init atom feed

---
+++ landing/src/pages/atom.xml.js
@@ -0,0 +1,21 @@
+import { Feed } from "feed";
+
+export async function GET(context) {
+    const feed = new Feed({
+        title: "Tembo's Blog",
+        description: "Latest news and technical blog posts from membors of the Tembo team and community!",
+        id: "https://tembo.io/blog",
+        link: "http://example.com/",
+        language: "en", // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+        image: "https://tembo.io/og-image.png",
+        favicon: "http://tembo.io/favicon.ico",
+        copyright: "All rights reserved 2024, Tembo",
+        updated: new Date(2013, 6, 14), // optional, default = today
+        generator: "awesome", // optional, default = 'Feed for Node.js'
+        feedLinks: {
+          atom: "https://tembo.io/atom.xml",
+          rss: "https://tembo.io/feed.xml"
+        },
+    });
+	return new Response(feed.atom1());
+}
