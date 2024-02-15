00771b2306fdc3c7f1f5bd82451720e3c66bd51e
Darren B <68653294+Devd0@users.noreply.github.com>
2024-02-15 09:03:39 -0500

    remove binary decoding for readFileSync

--- landing/src/pages/blog/[...slug].ts
+++ landing/src/pages/blog/[...slug].ts
@@ -1,15 +1,16 @@
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 import fs from 'fs';
-
+import path from 'path'
 
 export const GET: APIRoute = async ({ params, request }) => {
   const { slug } = params;
   const blog = (await getCollection('blog'))
   const post = blog.find((post) => post.slug === slug?.split('/')[0]);
   const directoryPath = `./src/content/blog/${post?.id.replace('/index.mdx', '').replace('/index.md', '')}/${slug?.split('/')[1]}`;
-  const fileContents = fs.readFileSync(directoryPath, 'binary');
-  return new Response(fileContents, { headers: { 'Content-Type': 'application/pdf' } });
+  const ext = path.extname(directoryPath);
+  const fileContents = fs.readFileSync(directoryPath);
+  return new Response(fileContents, { headers: { 'Content-Type': `application/${ext}` } });
 }
 
 export async function getStaticPaths() {
