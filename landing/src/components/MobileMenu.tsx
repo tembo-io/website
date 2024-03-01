8bcab1631ca0a72a06585217553f1c179f718877
Darren B <68653294+Devd0@users.noreply.github.com>
2024-02-29 21:26:24 -0500

    update mobile menu font sizing to fit extra link

--- landing/src/components/MobileMenu.tsx
+++ landing/src/components/MobileMenu.tsx
@@ -7,25 +7,32 @@ const MobileMenu = () => {
 		<div className='bg-offBlack mobile:hidden fixed z-10 w-screen h-screen overflow-hidden inset-0'>
 			<div className='bg-gradient-rainbow h-[4px] w-full' />
 			<Container styles='h-[100%] pb-12'>
-				<nav className='flex flex-col gap-[32px] h-full justify-between w-full'>
-					<div className='flex flex-col gap-[32px] mt-28 w-full'>
+				<nav className='flex flex-col gap-[20px] h-full justify-between w-full'>
+					<div className='flex flex-col gap-[20px] mt-28 w-full'>
 						<a
 							href='/'
-							className='font-secondary font-normal z-10 text-white text-[20px]'
+							className='font-secondary font-normal z-10 text-white text-[16px]'
 						>
 							Home
 						</a>
 						<img src={'/line.svg'} alt='line' />
 						<a
+							href='/pricing'
+							className='font-secondary font-normal z-10 text-white text-[16px]'
+						>
+							Pricing
+						</a>
+						<img src={'/line.svg'} alt='line' />
+						<a
 							href='/docs'
-							className='font-secondary font-normal z-10 text-white text-[20px]'
+							className='font-secondary font-normal z-10 text-white text-[16px]'
 						>
 							Docs
 						</a>
 						<img src={'/line.svg'} alt='line' />
 						<a
 							href='/blog'
-							className='font-secondary font-normal z-10 text-white text-[20px]'
+							className='font-secondary font-normal z-10 text-white text-[16px]'
 						>
 							Blog
 						</a>
@@ -34,7 +41,7 @@ const MobileMenu = () => {
 							href='https://github.com/tembo-io/tembo'
 							target='_blank'
 							rel='noreferrer'
-							className='font-secondary font-normal z-10 text-white text-[20px]'
+							className='font-secondary font-normal z-10 text-white text-[16px]'
 						>
 							<div className='flex gap-2 items-center'>
 								<img
