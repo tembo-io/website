8877a3c07cf80a5829e125fad3afcff4b9f0049e
Darren B <68653294+Devd0@users.noreply.github.com>
2024-03-04 13:34:45 -0500

    pricing tweaks

--- landing/src/components/Faqs.tsx
+++ landing/src/components/Faqs.tsx
@@ -9,24 +9,23 @@ const faqs = [
 	},
 	{
 		heading: 'When will I be billed?',
-		content:
-			'Lorem ipsum dolor sit amet consectetur. Massa eget dictumst senectus phasellus ultrices. Odio gravida mauris placerat suspendisse penatibus sit justo placerat praesent. Odio dapibus vitae magna nulla. Vulputate pharetra vitae pellentesque amet. Amet mi lacus et hendrerit est nunc quam arcu. Consectetur enim eu eget mauris commodo dictum egestas velit. Odio arcu turpis parturient semper sit congue cursus faucibus dui. Dolor viverra vel enim lorem. Auctor purus commodo tellus tristique id ut placerat. Hendrerit nunc vel elementum congue euismod enim cum ornare.',
+		content: 'Our Production plan charges at the end of each month.',
 	},
 	{
 		heading:
 			'Is the Hobby Tier free forever? What sort of workloads can I run on it?',
 		content:
-			'Lorem ipsum dolor sit amet consectetur. Massa eget dictumst senectus phasellus ultrices. Odio gravida mauris placerat suspendisse penatibus sit justo placerat praesent. Odio dapibus vitae magna nulla. Vulputate pharetra vitae pellentesque amet. Amet mi lacus et hendrerit est nunc quam arcu. Consectetur enim eu eget mauris commodo dictum egestas velit. Odio arcu turpis parturient semper sit congue cursus faucibus dui. Dolor viverra vel enim lorem. Auctor purus commodo tellus tristique id ut placerat. Hendrerit nunc vel elementum congue euismod enim cum ornare.',
+			'The Hobby Tier will be free forever. The purpose of the Hobby Tier is to prototype a side-project, to try out new use-cases with Tembo Cloud, or run some hobby projects ongoing but in an unsupported and ‘test’ sort of way.',
 	},
 	{
 		heading: 'Are annual plans available?',
 		content:
-			'Lorem ipsum dolor sit amet consectetur. Massa eget dictumst senectus phasellus ultrices. Odio gravida mauris placerat suspendisse penatibus sit justo placerat praesent. Odio dapibus vitae magna nulla. Vulputate pharetra vitae pellentesque amet. Amet mi lacus et hendrerit est nunc quam arcu. Consectetur enim eu eget mauris commodo dictum egestas velit. Odio arcu turpis parturient semper sit congue cursus faucibus dui. Dolor viverra vel enim lorem. Auctor purus commodo tellus tristique id ut placerat. Hendrerit nunc vel elementum congue euismod enim cum ornare.',
+			'Yes, please reach out to brad@tembo.io to inquire about annual plans that include hefty discounts.',
 	},
 	{
 		heading: 'Do you have plans specifically for early-stage startups?',
 		content:
-			'Lorem ipsum dolor sit amet consectetur. Massa eget dictumst senectus phasellus ultrices. Odio gravida mauris placerat suspendisse penatibus sit justo placerat praesent. Odio dapibus vitae magna nulla. Vulputate pharetra vitae pellentesque amet. Amet mi lacus et hendrerit est nunc quam arcu. Consectetur enim eu eget mauris commodo dictum egestas velit. Odio arcu turpis parturient semper sit congue cursus faucibus dui. Dolor viverra vel enim lorem. Auctor purus commodo tellus tristique id ut placerat. Hendrerit nunc vel elementum congue euismod enim cum ornare.',
+			'Yes! please visit our Tembo for Startups site (https://tembo.io/for-startups/) and follow instructions. We offer up to 50% off for the entire first year, and discounts ongoing for startups who are part of a Partner Organization in our Partner network.',
 	},
 	{
 		heading:
