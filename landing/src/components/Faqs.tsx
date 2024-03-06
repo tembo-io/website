eff4ec717794c2968634f18bd53ebed23786e398
Samay Sharma <samay@tembo.io>
2024-03-05 22:05:22 -0800

    Copy edits to the pricing page (#399)
    Copy edits to the pricing page
    
    Co-authored-by: Darren Baldwin <68653294+DarrenBaldwin07@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Darren Baldwin <68653294+DarrenBaldwin07@users.noreply.github.com>

--- landing/src/components/Faqs.tsx
+++ landing/src/components/Faqs.tsx
@@ -4,28 +4,28 @@ import Accordion from './Accordion';
 const faqs = [
 	{
 		heading: 'When will I be billed?',
-		content: 'Our Production plan charges at the end of each month.',
+		content: 'Our Production plan charges based on usage. You will receive your involve at the end of each month.',
 	},
 	{
 		heading:
 			'Is the Hobby Tier free forever? What sort of workloads can I run on it?',
 		content:
-			'The Hobby Tier will be free forever. The purpose of the Hobby Tier is to prototype a side-project, to try out new use-cases with Tembo Cloud, or run some hobby projects ongoing but in an unsupported and ‘test’ sort of way.',
+			'The Hobby Tier is free forever. The purpose of the Hobby Tier is to prototype a side-project and to try out different stacks and extensions on Tembo Cloud. We recommend using it for hobby projects but not for production workloads.',
 	},
 	{
 		heading: 'Are annual plans available?',
 		content:
-			'Yes, please reach out to brad@tembo.io to inquire about annual plans that include hefty discounts.',
+			'Yes, we do provide annual plans which offer a significant discount over the list price. Please reach out to brad@tembo.io to learn more.',
 	},
 	{
 		heading: 'Do you have plans specifically for early-stage startups?',
 		content:
-			'Yes! please visit our Tembo for Startups site (https://tembo.io/for-startups/) and follow instructions. We offer up to 50% off for the entire first year, and discounts ongoing for startups who are part of a Partner Organization in our Partner network.',
+			'Yes! please visit our Tembo for Startups site (https://tembo.io/for-startups/) and follow instructions. We offer up to 50% off for the first year, and ongoing discounts for startups who are part of a Partner Organization in our Partner network.',
 	},
 	{
 		heading: 'Can Tembo be hosted on my own infrastructure?',
 		content:
-			'Yes! Tembo is open-source on Github: https://github.com/tembo-io/tembo',
+			'Yes, you can self-host our Kubernetes Operator present at https://github.com/tembo-io/tembo/tree/main/tembo-operator or get in touch with brad@tembo.io to explore our private data plane offering.',
 	},
 ];
 
