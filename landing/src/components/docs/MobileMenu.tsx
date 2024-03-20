7b9b98f54bfd2fd81c9cf663c83cbbdee3e4c6b7
Darren B <68653294+Devd0@users.noreply.github.com>
2024-03-19 22:40:50 -0400

    start mobile menu

---
+++ landing/src/components/docs/MobileMenu.tsx
@@ -0,0 +1,19 @@
+import Container from '../Container';
+import type { SideBarSection } from '../../types';
+
+interface Props {
+	sideBarMenuSections: SideBarSection[];
+	isNestedSideBar: boolean;
+}
+
+const MobileMenu = () => {
+	return (
+		<div className='bg-offBlack mid:hidden fixed z-10 w-screen h-screen overflow-hidden inset-0'>
+			<Container styles='h-[100%] pb-12'>
+				<nav className='flex flex-col gap-[20px] h-full justify-between w-full'></nav>
+			</Container>
+		</div>
+	);
+};
+
+export default MobileMenu;
