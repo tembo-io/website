8c100a5e250d53ea539ae2ef555920256e43c60e
Joshua Jerin <joshuaxjerin@gmail.com>
2024-03-26 13:57:20 -0400

    update readme

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -88,12 +88,12 @@ const NavBar: React.FC<Props> = ({
 			)}
 		>
 			{isBanner && !isMenuOpen && (
-				<a href='/blog/postgres-14-and-16'>
+				<a href='/'>
 					<div
 						className={`flex items-center text-[12px] min-[400px]:text-sm justify-center gap-4 news-banner-container top-0 w-full text-center bg-[#131313] shadow-[0_-20px_36px_0_rgba(240,102,141,0.13)_inset] text-white px-[20px] mobile:px-[95px] py-3.5 sm:py-2.5 z-50`}
 					>
 						<span className='truncate'>
-							Announcing Support for Postgres 14 and 16
+							Your Announcements here
 						</span>
 						<span className='bg-gradient-to-r from-salmon via-purple to-lightPurple inline-block text-transparent bg-clip-text font-semibold text-sm whitespace-nowrap'>
 							Read more
