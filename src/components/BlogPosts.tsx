a0ce6495694b6f441e46a88c6ce96abfbb70bb8a
nishilfaldu <faldund@outlook.com>
2024-05-22 20:49:42 -0400

    feat(client): add search feature to blogposts based on title and content, CLOUD-422

--- src/components/BlogPosts.tsx
+++ src/components/BlogPosts.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useMemo, useState } from 'react';
 import { BlogPost } from './BlogPost';
 
 interface Post {
@@ -17,6 +18,21 @@ interface BlogPostsProps {
 }
 
 export function BlogPosts({ posts, children }: BlogPostsProps) {
+	const [filteredPosts, setFilteredPosts] = useState<Post[]>(posts);
+
+	const search = (query: string) => {
+		if (query.trim() === '') {
+			setFilteredPosts(posts);
+			return;
+		}
+		const filteredPosts_ = posts.filter(
+			(post) =>
+				post.title.toLowerCase().includes(query.trim().toLowerCase()) ||
+				post.body.toLowerCase().includes(query.trim().toLowerCase()),
+		);
+		setFilteredPosts(filteredPosts_);
+	};
+
 	return (
 		<div className='mt-16'>
 			<div className='flex justify-between max-[720px]:flex-col-reverse max-[720px]:gap-y-4'>
@@ -25,8 +41,8 @@ export function BlogPosts({ posts, children }: BlogPostsProps) {
 				</div>
 				<div className='relative'>
 					<input
-						className='bg-mwasi border border-[#EAEAEA33] rounded-full px-[6px] py-[6px] w-[400px] h-[42.57px] max-[720px]:w-full [@media(min-width:600px)]:h-[45px] flex items-center justify-between hover:cursor-pointer'
-						// onChange={}
+						className='bg-mwasi border border-[#EAEAEA33] rounded-full px-[6px] pl-11 py-[6px] w-[400px] h-[42.57px] max-[720px]:w-full [@media(min-width:600px)]:h-[45px] flex items-center justify-between hover:cursor-pointer'
+						onChange={(e) => search(e.target.value)}
 					/>
 					<div className='bg-white w-max p-2 rounded-full absolute top-2 left-2'>
 						<img
@@ -40,8 +56,8 @@ export function BlogPosts({ posts, children }: BlogPostsProps) {
 			</div>
 			<div className='mt-10 pb-2'>
 				<div className='grid grid-cols-12 lg:gap-6'>
-					{posts.length > 0 ? (
-						posts.map((post) => {
+					{filteredPosts.length > 0 ? (
+						filteredPosts.map((post) => {
 							return (
 								<BlogPost
 									title={post.title}
