7038cce0b498676d776e6da259c65a96e4126705
nishilfaldu <faldund@outlook.com>
2024-06-18 13:33:08 -0400

    feat(client): change tembo+stacks section, CLOUD-853

--- src/components/TemboStacksSection.tsx
+++ src/components/TemboStacksSection.tsx
@@ -17,7 +17,7 @@ const TemboStacksSection = () => {
 				/>
 				<div className='pt-28 z-10 relative'>
 					<h2 className='text-3xl customXxs:text-4xl customXs:text-6xl customMd:text-7xl text-white font-semibold tracking-[0.24px]'>
-						TEMBO + STACKS
+						ALL IN ONE PLATFORM
 					</h2>
 				</div>
 				<div className='flex flex-col-reverse mobile:flex-row justify-between items-start mt-20 gap-[50px] lg:gap-[150px] relative w-full'>
@@ -25,7 +25,7 @@ const TemboStacksSection = () => {
 						<DetailSequence
 							header='100% Open Source & Cloud Native'
 							text='The best of both worlds. Enjoy unmodified open source community Postgres and all the benefits of cloud native architecture, such as high availability, rolling updates, resource management, and more.'
-							iconPath='/cloudColorIcon.svg'
+							iconPath='/cloud-add-icon.svg'
 							styles='w-full max-w-none mobile:max-w-[530px] items-center mobile:items-start md:min-w-[350px] lg:min-w-[530px]'
 							headerStyles='text-center mobile:text-start'
 							textStyles='text-center mobile:text-start'
@@ -33,7 +33,7 @@ const TemboStacksSection = () => {
 						<DetailSequence
 							header='Fully Managed'
 							text='Deployment, configuration, management, and optimization are complicated. We handle them so you can focus on your application.'
-							iconPath='/boardColorIcon.svg'
+							iconPath='/candle-icon.svg'
 							styles='w-full max-w-none mobile:max-w-[530px] items-center mobile:items-start md:min-w-[350px] lg:min-w-[530px]'
 							headerStyles='text-center mobile:text-start'
 							textStyles='text-center mobile:text-start'
@@ -41,7 +41,7 @@ const TemboStacksSection = () => {
 						<DetailSequence
 							header='Customizable Security'
 							text='No two organizations are alike. Run securely in the cloud with with tools like user tiers, ip allow lists, and encrypted at-rest, or deploy in your own environment.'
-							iconPath='/cpuColorIcon.svg'
+							iconPath='/shield-icon.svg'
 							styles='w-full max-w-none mobile:max-w-[530px] items-center mobile:items-start md:min-w-[350px] lg:min-w-[530px]'
 							headerStyles='text-center mobile:text-start'
 							textStyles='text-center mobile:text-start'
@@ -49,7 +49,7 @@ const TemboStacksSection = () => {
 						<DetailSequence
 							header='Intuitive Interface'
 							text='Clear, elegant, delightful UI, because developers deserve nice things too. What’s more, our CLI-first system puts the power of Postgres in your hands. '
-							iconPath='/codeColorIcon.svg'
+							iconPath='/monitor-icon.svg'
 							styles='w-full max-w-none mobile:max-w-[530px] items-center mobile:items-start md:min-w-[350px] lg:min-w-[530px]'
 							headerStyles='text-center mobile:text-start'
 							textStyles='text-center mobile:text-start'
@@ -57,7 +57,7 @@ const TemboStacksSection = () => {
 						<DetailSequence
 							header='Performance Powered by Stacks'
 							text='Focus on your product, not your database. We optimize your whole environment—hardware, Postgres configs, and relevant extensions—to give you the best performance for your workload.'
-							iconPath='/mqColorIcon.svg'
+							iconPath='/cpu-charge-icon.svg'
 							styles='w-full max-w-none mobile:max-w-[530px] items-center mobile:items-start md:min-w-[350px] lg:min-w-[530px]'
 							headerStyles='text-center mobile:text-start'
 							textStyles='text-center mobile:text-start'
@@ -65,7 +65,7 @@ const TemboStacksSection = () => {
 						<DetailSequence
 							header='Transparent Pricing'
 							text='No hidden costs or surprise up-charges. You only pay for what you actually use. Store as much as you need, query as much as you want.'
-							iconPath='/priceColorIcon.svg'
+							iconPath='/coin-icon.svg'
 							styles='w-full max-w-none mobile:max-w-[530px] items-center mobile:items-start md:min-w-[350px] lg:min-w-[530px]'
 							headerStyles='text-center mobile:text-start'
 							textStyles='text-center mobile:text-start'
@@ -75,7 +75,7 @@ const TemboStacksSection = () => {
 							size='lg'
 							styles='w-max mt-10'
 						>
-							Try Free
+							Deploy now
 						</Button>
 					</div>
 					<img
