bcc9229687d211f682ad1b6430c6ea801ae364ad
aishwaryaborkar <ash@tembo.io>
2024-08-22 11:08:12 -0400

    round 1 of changes to match design spec

--- src/components/ArrowAccordion.tsx
+++ src/components/ArrowAccordion.tsx
@@ -56,7 +56,7 @@ const ArrowAccordion: React.FC<Props> = ({ items }) => {
 							</p>
 						</div>
 					</div>
-					<hr className='my-6 bg-[#EBEAE7]/[0.05]' />
+					<hr className='my-6 border-lightGrey h-[1px]' />
 				</li>
 			))}
 		</ul>
