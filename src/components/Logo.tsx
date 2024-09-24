b7b829227874b6764f089705c5d09c16ae52b449
aishwaryaborkar <ash@tembo.io>
2024-09-24 12:46:39 -0700

    add tm to logo in header, make headings on home page all title case

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,12 +4,17 @@ const Logo = () => {
 	return (
 		<a
 			href='/'
-			className='focus:outline-none transition hover:scale-105 duration-300 ease-in-out delay-70 z-50'
+			className='flex flex-row focus:outline-none transition hover:scale-105 duration-300 ease-in-out delay-70 z-50'
 		>
 			<img
 				src='/logoWithText.svg'
 				alt='tembo logo'
 				className='w-[105px] mobile:w-[124px]'
+			/>
+			<img
+				src='/trademark.svg'
+				alt='trademark'
+				className='mt-5 lg:mt-6 h-[5px] w-[7px]'
 			/>
 		</a>
 	);
