a8b7d04be3eb8d729c4a03477e878b48504d295c
Darren Baldwin <dmb.programmer@gmail.com>
2025-01-03 16:43:30 -0500

    <Copyright />

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Copyright = () => {
+	return (
+		<p>
+			©{new Date().getFullYear()} Tembo Data Systems, Inc. All Rights
+			Reserved.
+		</p>
+	);
+};
+
+export default Copyright;
